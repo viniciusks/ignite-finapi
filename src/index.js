d087d9b2d08cf14fd8cd68717efead124d0a4cea
vinicius kremer <vinicius.kremer@gmail.com>
2021-03-08 21:03:45 -0300

    First Commit in Dev

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get("/", (req, res) => {
+    return res.json({
+        message: "Hello World"
+    })
+})
+
 app.listen(3333, () => {
     console.log("Server is running!");
 });
