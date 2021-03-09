2296c806c886eeaa2b689bf165c6178da4cdff8e
vinicius kremer <vinicius.kremer@gmail.com>
2021-03-09 18:49:52 -0300

    Subindo alterações do account

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    return res.json({
-        message: "Hello World"
+const customers = [];
+
+app.get("/account", (req, res) => {
+    return res.json(customers);
+});
+
+// Accounts
+app.post('/account', (req, res) => {
+    const { cpf, name } = req.body;
+
+    const customersAlreadyExists = customers.some((customer) => customer.cpf == cpf);
+
+    const id = uuidv4();
+
+    if(!customersAlreadyExists) {
+        customers.push({
+            id: id,
+            cpf: cpf,
+            name: name,
+            statement: []
+        });
+        return res.status(201).json({
+            status: 201,
+            message: "Create successfully"
+        });
+    }
+
+    return res.status(400).json({
+        status: 400,
+        message: "Bad Request"
     })
-})
+
+});
 
 app.listen(3333, () => {
     console.log("Server is running!");
